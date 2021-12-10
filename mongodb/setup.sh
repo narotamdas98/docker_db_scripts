a66a168336054b39fb300eab56a7ec85dc639b64
abdulwahidgul24085 <abdulwahidgul@gmail.com>
2021-12-10 20:20:41 +0500

    command split

--- mongodb/setup.sh
+++ mongodb/setup.sh
@@ -1,4 +1,7 @@
 mkdir -p "$HOME/databases/mongodb/data"
 sudo chown 1000:1000 "$HOME/databases/mongodb/data"
 docker stop mongo_local && docker rm mongo_local
-docker run -d -p 27017:27017 --name mongo_local -v "$HOME/databases/mongodb/data":/data/db mvertes/alpine-mongo 
+docker run -d -p 27017:27017 \
+--name mongo_local \
+-v "$HOME/databases/mongodb/data":/data/db \
+--restart always mvertes/alpine-mongo 
--- postgres/setup.sh
+++ postgres/setup.sh
@@ -1,4 +1,8 @@
 mkdir -p "$HOME/databases/postgres/pgdata"
 sudo chown 1000:1000 "$HOME/databases/postgres/pgdata"
 docker stop pg_local && docker rm pg_local
-docker run -d -p 2345:5432 --name pg_local --user 1000:1000 -v "$HOME/databases/postgres/pgdata":/var/lib/postgresql/data/ -e POSTGRES_USER=awg -e POSTGRES_PASSWORD=pass --restart always postgres:latest
+docker run -d -p 2345:5432 --name pg_local \
+--user 1000:1000 \
+-v "$HOME/databases/postgres/pgdata":/var/lib/postgresql/data/ \
+-e POSTGRES_USER=awg -e POSTGRES_PASSWORD=pass \
+--restart always postgres:latest
